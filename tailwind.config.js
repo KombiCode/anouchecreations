5333e833892980b670c880615fac624970afdefa
Thierry Jet <thierry.jet@kombicode.com>
2022-04-08 11:57:24 +0200

    Add one custom font and one custom color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
+  mode: 'jit',
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandGolden: "#b39d24",
+        brandActive: "#b39d24"
+      },
+      fontFamily: {
+        anouche: ["Spartan", "sans-serif"]
+      }
+    },
+  }
 }
